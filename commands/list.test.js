1e10c39cb708172fe127d0e830e39b84fb3c4d31
Christian Fei <crifei93@gmail.com>
2020-09-30 16:07:13 +0200

    reversed test

--- commands/list.test.js
+++ commands/list.test.js
@@ -1,9 +1,17 @@
 const test = require('ava')
 const { handler } = require('./list')
 
-test('lists users specified via --username or in `~/.decent/usernames`', async t => {
-  const result = await handler({ username: 'lexfridman', max: 1 })
+test('lists users specified via --username, max 2', async t => {
+  const result = await handler({ username: 'lexfridman', max: 2 })
   t.truthy(result)
-  t.is(result.length, 1)
+  t.is(result.length, 2)
   t.is(result[0].user, 'lexfridman')
+  t.true(result[0].date < result[1].date)
+})
+
+test('lists users specified via --username, max 2, reversed', async t => {
+  const result = await handler({ username: 'lexfridman', max: 2, reverse: true })
+  t.truthy(result)
+  t.is(result.length, 2)
+  t.true(result[0].date > result[1].date)
 })
