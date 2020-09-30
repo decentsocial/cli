3b2f1479070bc1cee16602930a43118b4bafa0c5
Christian Fei <crifei93@gmail.com>
2020-09-30 16:16:53 +0200

    refactor init test

--- commands/init.js
+++ commands/init.js
@@ -10,14 +10,16 @@ exports.builder = {
 }
 exports.handler = function (argv, { mkdirSync, existsSync, writeFileSync } = fs) {
   console.log('creating directory ~/.decent')
-  if (existsSync(path.resolve(process.env.HOME, '.decent/usernames'))) {
+  const usernamesPath = path.resolve(process.env.HOME, '.decent/usernames')
+  if (existsSync(usernamesPath)) {
     console.log('~/.decent/usernames already exists')
     console.log('feel free to edit this file and follow users you care about')
     return false
   }
   mkdirSync('~/.decent', { recursive: true })
   console.log('creating usernames ~/.decent/usernames')
-  writeFileSync(path.resolve(process.env.HOME, '.decent/usernames'), argv.username.split(',').join('\n'), { encoding: 'utf-8' })
+  const content = argv.username.split(',').join('\n')
+  writeFileSync(usernamesPath, content, { encoding: 'utf-8' })
   console.log('successfully created ~/.decent/usernames\nfeel free to edit this file and follow users you care about')
   return true
 }
